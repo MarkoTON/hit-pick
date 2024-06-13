c407c74453a39cbb477732813f21839ea69b160a
markoton <markotoncanovic@gmail.com>
2024-06-13 17:52:22 +0200

    add some dumb components

--- src/app/components/vote-indicator/vote-indicator.component.ts
+++ src/app/components/vote-indicator/vote-indicator.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-vote-indicator',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './vote-indicator.component.scss'
 })
 export class VoteIndicatorComponent {
-
+  @Input() vote!: number | null;
 }
--- src/app/components/winning-vote-info/winning-vote-info.component.ts
+++ src/app/components/winning-vote-info/winning-vote-info.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-winning-vote-info',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './winning-vote-info.component.scss'
 })
 export class WinningVoteInfoComponent {
-
+  @Input() winningVoteCount!: number | null;
 }
