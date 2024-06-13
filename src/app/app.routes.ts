d32e77c3f53b2dc6bd7fa25cc725c9ff9cf370e1
markoton <markotoncanovic@gmail.com>
2024-06-13 13:57:07 +0200

    link pages and router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,16 @@
 import { Routes } from '@angular/router';
 import { RadioStationsComponent } from './pages/radio-stations/radio-stations.component';
+import { BestOfDayComponent } from './pages/best-of-day/best-of-day.component';
+import { BestOfMonthComponent } from './pages/best-of-month/best-of-month.component';
+import { MyListComponent } from './pages/my-list/my-list.component';
+import { UserComponent } from './components/user/user.component';
 
 export const routes: Routes = [
-  { path: '', component: RadioStationsComponent }
+  { path: '', pathMatch: 'full', component: RadioStationsComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'stations', component: RadioStationsComponent },
+  { path: 'best-of-day', component: BestOfDayComponent },
+  { path: 'best-of-month', component: BestOfMonthComponent },
+  { path: 'my-list', component: MyListComponent },
+  { path: '**', component: RadioStationsComponent },
 ];
