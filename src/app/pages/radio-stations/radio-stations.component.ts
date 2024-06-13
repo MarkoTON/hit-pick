1bc178414313fd34a18f1c54a27a28471f41d44d
markoton <markotoncanovic@gmail.com>
2024-06-13 11:05:13 +0200

    first rout and page added

--- src/app/pages/radio-stations/radio-stations.component.ts
+++ src/app/pages/radio-stations/radio-stations.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { QuickMenuComponent } from '../../components/quick-menu/quick-menu.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-radio-stations',
   standalone: true,
-  imports: [],
+  imports: [MatGridListModule, QuickMenuComponent, MatToolbarModule],
   templateUrl: './radio-stations.component.html',
   styleUrl: './radio-stations.component.scss'
 })
