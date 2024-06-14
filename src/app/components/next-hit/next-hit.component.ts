eaf69dba5c2eae283d59eb3c708b9c6bfe1ec951
markoton <markotoncanovic@gmail.com>
2024-06-14 11:36:27 +0200

    basic next hit structure

--- src/app/components/next-hit/next-hit.component.ts
+++ src/app/components/next-hit/next-hit.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SongsListComponent } from '../songs-list/songs-list.component';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-next-hit',
   standalone: true,
-  imports: [],
+  imports: [SongsListComponent, MatCardModule],
   templateUrl: './next-hit.component.html',
   styleUrl: './next-hit.component.scss'
 })
