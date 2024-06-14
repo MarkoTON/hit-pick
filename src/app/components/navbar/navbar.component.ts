38b31ebc87420395714f22434db1b4d4db6f46ed
markoton <markotoncanovic@gmail.com>
2024-06-14 13:25:23 +0200

    reorder 1 level

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { SidebarService } from '../sidebar/sidebar.service';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { SidebarService } from '../sidebar/sidebar.service';
 
 @Component({
   selector: 'app-navbar',
